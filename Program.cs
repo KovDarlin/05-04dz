0d0d775f33f82308a8efb5eb8e4a2809413a705f
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-03-02 21:35:51 +0200

    ex 6 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System;
+
+class Program
+{
+    delegate void NumberProces(int number);
+
+    static void Main()
+    {
+        NumberProces proces = Positive;
+        proces += Even;
+        proces += Check;
+
+        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            proces(number);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+
+    static void Positive(int number)
+    {
+        if (number > 0)
+            Console.WriteLine("The number is positive.");
+        else
+            Console.WriteLine("The number is not positive.");
+    }
+
+    static void Even(int number)
+    {
+        if (number % 2 == 0)
+            Console.WriteLine("The number is even.");
+        else
+            Console.WriteLine("The number is odd.");
+    }
+
+    static void Check(int number)
+    {
+        if (number % 7 == 0)
+            Console.WriteLine("The number is a multiple of 7.");
+        else
+            Console.WriteLine("The number is not a multiple of f7.");
+    }
+}
